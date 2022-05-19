5660d5abc4d8fe75edc91676181bde39c1038382
vitorozman <64697421+vitorozman@users.noreply.github.com>
2022-05-19 14:32:46 +0200

    Dodali okno za transakcije in popravek uporabnika

--- gama.py
+++ gama.py
@@ -3,6 +3,9 @@ from bottle import *
 from auth import *
 import hashlib
 from bottleext import *
+from datetime import date
+
+
 
 
 import psycopg2, psycopg2.extensions, psycopg2.extras
@@ -129,10 +132,6 @@ def prijava_post():
     redirect(url('uporabnik_get', id_uporabnika=id_uporabnika))
 
 
-
-
-
-
 @get('/uporabnik/<id_uporabnika>')
 def uporabnik_get(id_uporabnika):
     id_uporabnika = int(id_uporabnika)
@@ -234,40 +233,42 @@ def uporabnik_post(id_uporabnika):
         
 
 
-@get('/transakcija/<id_uporabnika>')
-def trasakcija_get(id_uporabnika):
-    #tu bomo naredili transakcijo
-    return "blalalal"
-    #cur.execute("SELECT osnovna_valuta FROM devizni_tecaj GROUP BY osnovna_valuta ORDER BY osnovna_valuta")
-    #valute = sum(cur.fetchall(), [])
-    #cur.execute("SELECT * FROM transakcija")
-    #trans = cur.fetchall()
-    #return template("testiranje.html", naslov = "transakcija", valute = valute, trans=trans)
- #Če dopišemo reloader=True, se bo sam restartal vsakič, ko spremenimo datoteko
+@get('/transakcija/<id_uporabnika>/<borza>')
+def transakcija_get(id_uporabnika, borza):
+    today = date.today()
+    datum = today.strftime("%Y-%m-%d")
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT id_borze FROM borza WHERE ime = %s", [borza])
+    borza_id = cur.fetchone()[0]
+
+    cur.execute('''
+    SELECT v_valuto FROM transakcija 
+    WHERE uporabnik_id=%s AND borza_id = %s AND v_valuto <> ''
+    GROUP BY v_valuto
+    ''', [int(id_uporabnika), int(borza_id)])
+    valute = cur.fetchall()
+    #valute = [['USD'], ['BTC'], ['ETH']]    
+    return template("transakcija.html", naslov ="Transakcija", borza=borza, datum=datum, valute=valute, id_uporabnika=id_uporabnika)
 
 
-@post('/transakcija')
-def dodaj_transakcijo():
-    crypto = str(request.forms.get('crypto'))
-    iz_kolicine = int(request.forms.get('iz_kolicine'))
-    v_kolicino = int(request.forms.get('v_kolicino'))
+@post('/transakcija/<id_uporabnika>')
+def transkacija_post(id_uporabnika):
+    id_uporabnika = int(id_uporabnika)
+    iz_valute = request.forms.get('iz_valute')
+    v_valuto = request.forms.get('v_valuto')
+    kolicina = request.forms.get('kolicina')
+    datum_transakcije = request.forms.get('datum_transakcije')
+    ime_borze = request.forms.get('ime_borze')
 
-    cur.execute("""SELECT id_transakcije FROM transakcija
-                    ORDER BY id_transakcije""")
-    kljuci = sum(cur.fetchall(), [])
-    if len(kljuci) == 0:
-        id_transakcije = 1
-    else:
-        id_transakcije = kljuci.pop() + 1
-    cur.execute("""
-    INSERT INTO transakcija (id_transakcije, iz_kolicine, v_kolicino, iz_valute, v_valuto)
-    VALUES (%s, %s, %s, %s, %s)"""
-    , [id_transakcije, iz_kolicine, v_kolicino, 'USD', crypto])
-    redirect('/test')
+    print(id_uporabnika, iz_valute, v_valuto, kolicina, datum_transakcije, ime_borze)
+    #.....
+    return 
+    
 
-@get('/denarnica')
-def denarnica():
-    pass
+@get('/D_W')
+def D_W_get():
+    # tu bomo naredili poizvedbo za dodajenaje D in W
+    return
 
 
 

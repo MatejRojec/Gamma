c10f77e457f7a49bd7339e13a4cd2bdd829a0e04
vitorozman <64697421+vitorozman@users.noreply.github.com>
2022-05-19 14:31:54 +0200

    Poizvedba za stanje z trenutnim deviznim

--- code.sql
+++ code.sql
@@ -58,3 +58,55 @@ WHERE t2.valuta <> '')
 SELECT * FROM t3 
 UNION 
 SELECT * FROM t4
+
+
+
+
+-- dodatek z trenutnim tecajem v USD
+WITH t1 AS (SELECT uporabnik_id,
+                    borza_id,
+                    iz_valute AS valuta,
+                    sum(iz_kolicine) AS x
+FROM transakcija AS trx
+WHERE uporabnik_id = 1001
+GROUP BY 1, 2, 3),
+t2 AS (SELECT uporabnik_id,
+               borza_id,
+               v_valuto AS valuta,
+               sum(v_kolicino) AS y
+FROM transakcija AS trx
+WHERE uporabnik_id = 1001 
+GROUP BY 1, 2, 3),
+t3 AS (SELECT t1.uporabnik_id,
+               b.ime,
+               t1.valuta, 
+               COALESCE(y, 0) - COALESCE(x, 0) AS amount       
+FROM t1 
+     FULL JOIN t2 ON t1.uporabnik_id = t2.uporabnik_id 
+          AND t1.valuta = t2.valuta 
+          AND t1.borza_id = t2.borza_id
+     LEFT JOIN borza AS b ON b.id_borze = t1.borza_id
+WHERE t1.valuta <> ''),
+t4 AS (SELECT t2.uporabnik_id,
+               b.ime,
+               t2.valuta, 
+               COALESCE(y, 0) - COALESCE(x, 0) AS amount       
+FROM t2 
+     FULL JOIN t1 ON t1.uporabnik_id = t2.uporabnik_id 
+          AND t1.valuta = t2.valuta 
+          AND t1.borza_id = t2.borza_id
+     LEFT JOIN borza AS b ON b.id_borze = t2.borza_id
+WHERE t2.valuta <> ''),
+t5 as(     
+SELECT * FROM t3 
+UNION 
+SELECT * FROM t4)
+SELECT t5.uporabnik_id,
+       t5.ime,
+       t5.valuta,
+       t5.amount,
+       t5.amount * COALESCE(valutno_razmerje, 1) as amount_usd
+        FROM t5 
+            left join devizni_tecaj dt
+                on dt.osnovna_valuta = t5.valuta
+                AND date(datum_razmerja) = date(now())
